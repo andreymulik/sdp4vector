b35d00f09aa1c9f4ea63e05890e101e2d51cfff4
Andrey Mulik <work.a.mulik@gmail.com>
2019-11-11 19:17:20 +0300

    added new instances and test suite

--- src/SDP/Vector.hs
+++ src/SDP/Vector.hs
@@ -1,10 +1,21 @@
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
 
+{- |
+    Module      :  SDP.Vector
+    Copyright   :  (c) Andrey Mulik 2019
+    License     :  BSD-style
+    Maintainer  :  work.a.mulik@gmail.com
+    Portability :  non-portable (GHC extensions)
+    
+    @SDP.Vector@ provides 'Vector' - immutable lazy boxed vector.
+-}
 module SDP.Vector
 (
+  -- * Exports
   module SDP.Indexed,
   -- module SDP.Sort,
   
+  -- * Vector
   Vector
 )
 where
@@ -12,15 +23,48 @@ where
 import Prelude ()
 import SDP.SafePrelude
 
+import Control.Monad.ST ( runST, ST )
+
+import SDP.IndexedM
 import SDP.Indexed
--- import SDP.Sort
+import SDP.Sort
+import SDP.Scan
 
 import Data.Vector ( Vector )
 import qualified Data.Vector as V
 
+import SDP.Array.ST
+import SDP.Unrolled.ST
+import SDP.Unrolled.STUnlist
+
+import SDP.Array.IO
+import SDP.Unrolled.IO
+import SDP.Unrolled.IOUnlist
+
+import SDP.SortM.Tim
+
 default ()
 
 --------------------------------------------------------------------------------
+
+{- Zip, Scan and Estimate instances. -}
+
+instance Zip Vector
+  where
+    zipWith  = V.zipWith
+    zipWith3 = V.zipWith3
+    zipWith4 = V.zipWith4
+    zipWith5 = V.zipWith5
+    zipWith6 = V.zipWith6
+
+instance Scan Vector
+  where
+    scanl = V.scanl
+    scanr = V.scanr
+    
+    scanl' = V.scanl'
+    scanl1 = V.scanl1
+    scanr1 = V.scanr1
 
 instance Estimate (Vector e)
   where
@@ -41,15 +85,9 @@ instance Estimate (Vector e)
     xs .==. ys = sizeOf xs == sizeOf ys
     xs ./=. ys = sizeOf xs /= sizeOf ys
 
-instance Zip Vector
-  where
-    zipWith  = V.zipWith
-    zipWith3 = V.zipWith3
-    zipWith4 = V.zipWith4
-    zipWith5 = V.zipWith5
-    zipWith6 = V.zipWith6
+--------------------------------------------------------------------------------
 
--- instance Scan
+{- Linear, Split and Bordered instances. -}
 
 instance Linear (Vector e) e
   where
@@ -101,12 +139,13 @@ instance Bordered (Vector e) Int e
     
     sizeOf = V.length
 
+--------------------------------------------------------------------------------
+
+{- Indexed, IFold and Sort instances. -}
+
 instance Indexed (Vector e) Int e
   where
-    assoc' bnds@(l, _) defvalue ascs = accum (flip const) es ies
-      where
-        ies = [ (i - l, e) | (i, e) <- ascs, inRange bnds i ]
-        es  = replicate (size bnds) defvalue
+    assoc' bnds defvalue ascs = runST $ fromAssocs' bnds defvalue ascs >>= done
     
     fromIndexed es' = accum (flip const) es ies
       where
@@ -119,7 +158,11 @@ instance Indexed (Vector e) Int e
     (!?) = (V.!?)
     (!)  = (V.!)
     
-    (//) = (V.//)
+    Z  // ascs = null ascs ? Z $ assoc (l, u) ascs
+      where
+        l = fst $ minimumBy cmpfst ascs
+        u = fst $ maximumBy cmpfst ascs
+    vs // ascs = vs V.// ascs
     
     (.$) = V.findIndex
     (*$) = \ p -> toList . V.findIndices p
@@ -132,9 +175,36 @@ instance IFold (Vector e) Int e
     i_foldl = foldl
     i_foldr = foldr
 
--- instance Sort (Vector e) e {- Soon. -}
+instance Sort (Vector e) e
+  where
+    sortBy cmp es = runST $ do es' <- thaw es; timSortBy cmp es'; done es'
+
+--------------------------------------------------------------------------------
+
+{- Thaw and Freeze instances. -}
+
+instance Thaw (ST s) (Vector e) (STArray    s Int e) where thaw = fromFoldableM
+instance Thaw (ST s) (Vector e) (STUnlist   s     e) where thaw = fromFoldableM
+instance Thaw (ST s) (Vector e) (STUnrolled s Int e) where thaw = fromFoldableM
+
+instance Thaw IO (Vector e) (IOArray    Int e) where thaw = fromFoldableM
+instance Thaw IO (Vector e) (IOUnlist       e) where thaw = fromFoldableM
+instance Thaw IO (Vector e) (IOUnrolled Int e) where thaw = fromFoldableM
 
 
+
+instance Freeze (ST s) (STArray    s Int e) (Vector e) where freeze = fmap fromList . getLeft
+instance Freeze (ST s) (STUnlist   s     e) (Vector e) where freeze = fmap fromList . getLeft
+instance Freeze (ST s) (STUnrolled s Int e) (Vector e) where freeze = fmap fromList . getLeft
+
+instance Freeze IO (IOArray    Int e) (Vector e) where freeze = fmap fromList . getLeft
+instance Freeze IO (IOUnlist       e) (Vector e) where freeze = fmap fromList . getLeft
+instance Freeze IO (IOUnrolled Int e) (Vector e) where freeze = fmap fromList . getLeft
+
+--------------------------------------------------------------------------------
+
+done :: STArray s Int e -> ST s (Vector e)
+done =  freeze
 
 
 
---
+++ test/test-vector-boxed.hs
@@ -0,0 +1,120 @@
+module Main where
+
+import Test.Framework
+import Test.Framework.Providers.QuickCheck2
+
+import SDP.Vector
+import SDP.Vector.Related ()
+
+import Test.SDP
+
+default ()
+
+--------------------------------------------------------------------------------
+
+main :: IO ()
+main = defaultMain
+  [
+    -- common tests
+    testProperty "vector-boxed-eq             " eqProp,
+    testProperty "vector-boxed-ord            " ordProp,
+    testProperty "vector-boxed-lexicographic  " lgoProp,
+    
+    -- linear tests
+    testProperty "vector-boxed-linear-basic   " basicLinearProp,
+    testProperty "vector-boxed-linear-decons  " deconstructionLinearProp,
+    testProperty "vector-boxed-linear-cons    " constructionLinearProp,
+    testProperty "vector-boxed-linear-reverse " reverseProp,
+    testProperty "vector-boxed-linear-concat  " concatProp,
+    
+    -- split test
+    testProperty "vector-boxed-split          " splitProp,
+    
+    -- indexed tests
+    testProperty "vector-boxed-indexed-basic  " basicIndexedProp,
+    testProperty "vector-boxed-indexed-assoc  " assocIndexedProp,
+    testProperty "vector-boxed-indexed-read   " readIndexedProp,
+    
+    -- sort test
+    testProperty "vector-boxed-sort           " sortProp,
+    
+    -- estimate test
+    testProperty "vector-boxed-estimate       " estimateProp
+  ]
+
+--------------------------------------------------------------------------------
+
+{- Eq property. -}
+
+eqProp :: TestEq (Vector Int)
+eqProp =  eqTest
+
+--------------------------------------------------------------------------------
+
+{- Ord property. -}
+
+ordProp :: TestOrd (Vector Int)
+ordProp =  ordTest
+
+lgoProp :: Long (Vector Int) -> Long (Vector Int) -> Bool
+lgoProp (Long xs) (Long ys) = lexicographicOrdTest xs ys
+
+--------------------------------------------------------------------------------
+
+{- Linear properties. -}
+
+basicLinearProp          :: Int -> Vector Int -> Bool
+basicLinearProp          =  basicLinearTest
+
+deconstructionLinearProp :: Vector Int -> Bool
+deconstructionLinearProp =  deconstructionLinearTest
+
+constructionLinearProp   :: Int -> Vector Int -> Bool
+constructionLinearProp   =  constructionLinearTest
+
+reverseProp              :: Vector Int -> Bool
+reverseProp              =  reverseTest
+
+replicateProp            :: TestLinear (Vector Int) Int
+replicateProp            =  replicateTest
+
+concatProp               :: (Vector Int) -> Bool
+concatProp               =  concatTest
+
+--------------------------------------------------------------------------------
+
+{- Split property. -}
+
+splitProp :: TestSplit (Vector Int)
+splitProp =  splitTest
+
+--------------------------------------------------------------------------------
+
+{- Indexed property. -}
+
+basicIndexedProp :: TestIndexed1 Vector Int Int
+basicIndexedProp =  basicIndexedTest
+
+assocIndexedProp :: TestIndexed1 Vector Int Int
+assocIndexedProp =  assocIndexedTest
+
+readIndexedProp  :: TestIndexed1 Vector Int Int
+readIndexedProp  =  readIndexedTest
+
+--------------------------------------------------------------------------------
+
+{- Sort property. -}
+
+sortProp :: Medium (Vector Int) -> Bool
+sortProp =  sortTest
+
+--------------------------------------------------------------------------------
+
+{- Estimate property. -}
+
+estimateProp :: TestEstimate (Vector Int)
+estimateProp =  estimateTest
+
+
+
+
